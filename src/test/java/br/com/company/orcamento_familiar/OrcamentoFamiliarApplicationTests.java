e0a7c0587fd6198edad04fe99a4814aec344e0b9
gustavoestevesr <gustavo.tech4me@gmail.com>
2022-11-11 11:28:35 -0300

    first commit

---
+++ src/test/java/br/com/company/orcamento_familiar/OrcamentoFamiliarApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.company.orcamento_familiar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrcamentoFamiliarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
